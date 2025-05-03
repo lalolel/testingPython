ad4889dcd13b18dd84e02e03351915fef8bf38a8
Lalo <114935993+lalolel@users.noreply.github.com>
2025-05-03 14:42:18 +0300

    Update and rename sumELse.py to pigLatin.py

--- testingElseIf/pigLatin.py
+++ testingElseIf/pigLatin.py
@@ -6,3 +6,9 @@ if len(original) > 0:
   print original
 else:
   print "empty"
+
+original = raw_input("Enter a word:")
+if len(original) > 0 and original.isalpha():
+  print original
+else:
+  print "empty"
