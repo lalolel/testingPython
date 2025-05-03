5e2cc06224909fb19228444930a9bad23a2e6588
Lalo <114935993+lalolel@users.noreply.github.com>
2025-05-03 15:49:15 +0300

    Update pigLatin.py

--- testingElseIf/pigLatin.py
+++ testingElseIf/pigLatin.py
@@ -46,3 +46,15 @@ if len(original) > 0 and original.isalpha():
   new_word = new_word[1:len(new_word)]
 else:
     print 'empty'
+
+pyg = 'ay'
+
+original = raw_input('Enter a word:')
+
+if len(original) > 0 and original.isalpha():
+  word = original.lower()
+  first = word[0]
+  new_word = word + first + pyg
+  new_word = new_word[1:len(new_word)]
+else:
+    print 'empty'
