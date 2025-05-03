561a3a044d93c004a8deb18ee8eefa1865a8d5f9
Lalo <114935993+lalolel@users.noreply.github.com>
2025-05-03 14:31:44 +0300

    Update flow2.py

--- testingElseIf/flow2.py
+++ testingElseIf/flow2.py
@@ -24,3 +24,26 @@ def greater_less_equal_5(answer):
 print greater_less_equal_5(4)
 print greater_less_equal_5(5)
 print greater_less_equal_5(6)
+
+
+# Complete the if and elif statements!
+def grade_converter(grade):
+    if grade >= 90:
+        return "A"
+    elif grade >= 80:
+        return "B"
+    elif grade >= 70:
+        return "C"
+    elif grade >= 65:
+        return "D"
+    else:
+        return "F"
+      
+# This should print an "A"      
+print grade_converter(92)
+
+# This should print a "C"
+print grade_converter(70)
+
+# This should print an "F"
+print grade_converter(61)
