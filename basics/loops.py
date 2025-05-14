5f86effc89e2b0af4d035b1676c2533cb8c7a909
Lalo <114935993+lalolel@users.noreply.github.com>
2025-05-14 13:12:30 +0300

    Update loops.py

--- basics/loops.py
+++ basics/loops.py
@@ -24,3 +24,28 @@ def join_strings(words):
   return result
 
 print join_strings(n)
+
+
+m = [1, 2, 3]
+n = [4, 5, 6]
+
+# Add your code here!
+
+def join_lists(x, y):
+	return x + y
+
+
+print join_lists(m, n)
+# You want this to print [1, 2, 3, 4, 5, 6]
+
+n = [[1, 2, 3], [4, 5, 6, 7, 8, 9]]
+# Add your function here
+
+def flatten(lists):
+  results = []
+  for numbers in lists:
+    for number in numbers:
+      results.append(number)
+  return results
+
+print flatten(n)
