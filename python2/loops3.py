6f68487d6f920aa661743d753a613fafce81d9f6
Lalo <114935993+lalolel@users.noreply.github.com>
2025-05-17 23:57:32 +0300

    Create loops3.py

---
+++ python2/loops3.py
@@ -0,0 +1,125 @@
+def digit_sum(n):
+  total = 0
+  string_n = str(n)
+  for char in string_n:
+    total += int(char)
+  return total
+
+#Alternate Solution:
+
+#def digit_sum(n):
+#  total = 0
+#  while n > 0:
+#    total += n % 10
+#    n = n // 10
+#  return total
+  
+print digit_sum(1234)
+
+def factorial(x):
+    total = 1
+    while x>0:
+        total *= x
+        x-=1
+    return total
+  
+print factorial(5)
+
+def is_prime(x):
+    if x < 2:
+        return False
+    else:
+        for n in range(2, x-1):
+            if x % n == 0:
+                return False
+        return True
+
+print is_prime(13)
+print is_prime(10)
+
+  def reverse(text):
+    word = ""
+    l = len(text) - 1
+    while l >= 0:
+        word = word + text[l]
+        l -= 1
+    return word
+  
+print reverse("Hello World")
+
+def anti_vowel(text):
+    result = ""
+    vowels = "ieaouIEAOU"
+    for char in text:
+          if char not in vowels:
+            result += char
+    return result
+print anti_vowel("hello book")
+
+score = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2, 
+         "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3, 
+         "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1, 
+         "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4, 
+         "x": 8, "z": 10}
+         
+def scrabble_score(word):
+  word = word.lower()
+  total = 0
+  for letter in word:
+    for leter in score:
+      if letter == leter:
+        total = total + score[leter]
+  return total
+
+print scrabble_score("pizza")
+
+def censor(text, word):
+    words = text.split()
+    result = ''
+    stars = '*' * len(word)
+    count = 0
+    for i in words:
+        if i == word:
+            words[count] = stars
+        count += 1
+    result =' '.join(words)
+
+    return result
+  
+print censor("this hack is wack hack", "hack")
+
+def count(sequence, item):
+    count = 0
+    for i in sequence:
+        if i == item:
+            count += 1
+    return count
+  
+print count([1, 2, 1, 1], 1)
+
+
+''' Define a function called purify that takes in a list of numbers, removes all odd numbers in the list, and returns the result. For example, purify([1,2,3]) should return [2].
+
+Do not directly modify the list you are given as input; instead, return a new list with only the even numbers. '''
+
+def purify(lst):
+    res = []
+    for ele in lst:
+        if ele % 2 == 0:
+            res.append(ele)
+    return res
+  
+print purify([1, 2, 3, 4])
+
+''' Define a function called product that takes a list of integers as input and returns the product of all of the elements in the list. For example: product([4, 5, 5]) should return 100 (because 4 * 5 * 5 is 100).
+
+Don’t worry about the list being empty.
+Your function should return an integer.
+ '''
+def product(list):
+  total = 1
+  for num in list:
+    total = total * num
+  return total
+
+print product([4, 5, 5])
