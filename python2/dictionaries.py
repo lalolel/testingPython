aaf7bf776b66c8269eef74c38b1088a8f0a404fd
Lalo <114935993+lalolel@users.noreply.github.com>
2025-05-18 04:44:41 +0300

    Update dictionaries.py

--- python2/dictionaries.py
+++ python2/dictionaries.py
@@ -33,3 +33,11 @@ doubles_by_3 = [x * 2 for x in range(1, 6) if (x * 2) % 3 == 0]
 even_squares = [x ** 2 for x in range(1, 12) if x % 2 == 0]
 
 print even_squares
+
+cubes_by_four = [x ** 3 for x in range(1, 11) if ((x ** 3) % 4) == 0]
+print cubes_by_four
+
+l = [i ** 2 for i in range(1, 11)]
+# Should be [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+print l[2:9:2]
