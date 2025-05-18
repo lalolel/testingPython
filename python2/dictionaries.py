79b98558f3dc00d24697a7b2901142ab1ad063f9
Lalo <114935993+lalolel@users.noreply.github.com>
2025-05-18 19:35:30 +0300

    Update dictionaries.py

--- python2/dictionaries.py
+++ python2/dictionaries.py
@@ -61,7 +61,11 @@ to_one_hundred = range(101)
 
 backwards_by_tens = to_one_hundred[::-10]
 print backwards_by_tens
+''' Create a list, to_21, that’s just the numbers from 1 to 21, inclusive.
 
+Create a second list, odds, that contains only the odd numbers in the to_21 list (1, 3, 5, and so on). Use list slicing for this one instead of a list comprehension.
+
+Finally, create a third list, middle_third, that’s equal to the middle third of to_21, from 8 to 14, inclusive. '''
 
 to_21 = range(1, 22)
 
