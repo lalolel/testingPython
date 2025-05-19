3b60112df2a6af223d8bf569c53348642b490349
Lalo <114935993+lalolel@users.noreply.github.com>
2025-05-19 21:52:35 +0300

    Update classes.py

--- python2/classes.py
+++ python2/classes.py
@@ -181,3 +181,45 @@ class PartTimeEmployee(Employee):
   def calculate_wage(self, hours):
     self.hours = hours
     return hours * 12.00
+
+
+class Employee(object):
+  """Models real-life employees!"""
+  def __init__(self, employee_name):
+    self.employee_name = employee_name
+
+  def calculate_wage(self, hours):
+    self.hours = hours
+    return hours * 20.00
+
+# Add your code below!
+class PartTimeEmployee(Employee):
+  def calculate_wage(self, hours):
+    self.hours = hours
+    return hours * 12.00
+  
+  def full_time_wage(self, hours):
+    return super(PartTimeEmployee, self).calculate_wage(hours)
+
+milton = PartTimeEmployee('Milton')
+print milton.full_time_wage(10)
+
+class Triangle(object):
+  number_of_sides = 3
+  def __init__(self, angle1, angle2, angle3):
+    self.angle1 = angle1
+    self.angle2 = angle2
+    self.angle3 = angle3
+    
+  def check_angles(self):
+    if (self.angle1 + self.angle2 + self.angle3) == 180:
+      return True
+    else:
+      return False
+    
+class Equilateral(Triangle):
+  angle = 60
+  def __init__(self):
+    self.angle1 = self.angle
+    self.angle2 = self.angle
+    self.angle3 = self.angle
