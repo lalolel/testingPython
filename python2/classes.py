4b8b412fef67af484585c7e4082e0cd8fc49ba75
Lalo <114935993+lalolel@users.noreply.github.com>
2025-05-19 21:46:00 +0300

    Update classes.py

--- python2/classes.py
+++ python2/classes.py
@@ -150,3 +150,34 @@ class Triangle(Shape):
     self.side1 = side1
     self.side2 = side2
     self.side3 = side3
+
+class Employee(object):
+  def __init__(self, name):
+    self.name = name
+  def greet(self, other):
+    print "Hello, %s" % other.name
+
+class CEO(Employee):
+  def greet(self, other):
+    print "Get back to work, %s!" % other.name
+
+ceo = CEO("Emily")
+emp = Employee("Steve")
+emp.greet(ceo)
+# Hello, Emily
+ceo.greet(emp)
+# Get back to work, Steve!
+class Employee(object):
+  """Models real-life employees!"""
+  def __init__(self, employee_name):
+    self.employee_name = employee_name
+
+  def calculate_wage(self, hours):
+    self.hours = hours
+    return hours * 20.00
+
+# Add your code below!
+class PartTimeEmployee(Employee):
+  def calculate_wage(self, hours):
+    self.hours = hours
+    return hours * 12.00
