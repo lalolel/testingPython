bc13a2611285af018691541792b219cffb3e7d9e
Lalo <114935993+lalolel@users.noreply.github.com>
2025-05-19 19:57:16 +0300

    Update classes.py

--- python2/classes.py
+++ python2/classes.py
@@ -28,3 +28,125 @@ You can think of __init__() as the function that “boots up” each object the 
 class Animal(object):
   def __init__(self):
     pass
+    
+class Animal(object):
+  def __init__(self, name):
+    self.name = name
+    
+zebra = Animal("Jeffrey")
+
+print zebra.name
+
+# Class definition
+class Animal(object):
+  """Makes cute animals."""
+  # For initializing our instance objects
+  def __init__(self, name, age, is_hungry):
+    self.name = name
+    self.age = age
+    self.is_hungry = is_hungry
+
+# Note that self is only used in the __init__()
+# function definition; we don't need to pass it
+# to our instance objects.
+
+zebra = Animal("Jeffrey", 2, True)
+giraffe = Animal("Bruce", 1, False)
+panda = Animal("Chad", 7, True)
+
+print zebra.name, zebra.age, zebra.is_hungry
+print giraffe.name, giraffe.age, giraffe.is_hungry
+print panda.name, panda.age, panda.is_hungry
+
+
+class Animal(object):
+  """Makes cute animals."""
+  is_alive = True
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+  # Add your method here!
+  def description(self):
+    print self.name
+    print self.age
+    
+hippo = Animal("Anderson", 36)
+hippo.description()
+
+
+class Animal(object):
+  """Makes cute animals."""
+  is_alive = True
+  health = "good"
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+  # Add your method here!
+  def description(self):
+    print self.name
+    print self.age
+    
+hippo = Animal('Anderson', 36)
+sloth = Animal('Dale', 15)
+ocelot = Animal('Fuzzy', 7)
+
+print hippo.health
+print sloth.health
+print ocelot.health
+
+
+class ShoppingCart(object):
+  """Creates shopping cart objects
+  for users of our fine website."""
+  
+  def __init__(self, customer_name):
+    self.customer_name = customer_name
+    self.items_in_cart = {}
+  def add_item(self, product, price):
+    """Add product to the cart."""
+    if not product in self.items_in_cart:
+      self.items_in_cart[product] = price
+      print product + " added."
+    else:
+      print product + " is already in the cart."
+
+  def remove_item(self, product):
+    """Remove product from the cart."""
+    if product in self.items_in_cart:
+      del self.items_in_cart[product]
+      print product + " removed."
+    else:
+      print product + " is not in the cart."
+
+my_cart = ShoppingCart("Eric")
+my_cart.add_item("Ukelele", 10)
+
+class Customer(object):
+  """Produces objects that represent customers."""
+  def __init__(self, customer_id):
+    self.customer_id = customer_id
+
+  def display_cart(self):
+    print "I'm a string that stands in for the contents of your shopping cart!"
+
+class ReturningCustomer(Customer):
+  """For customers of the repeat variety."""
+  def display_order_history(self):
+    print "I'm a string that stands in for your order history!"
+
+monty_python = ReturningCustomer("ID: 12345")
+monty_python.display_cart()
+monty_python.display_order_history()
+
+
+class Shape(object):
+  """Makes shapes!"""
+  def __init__(self, number_of_sides):
+    self.number_of_sides = number_of_sides
+
+# Add your Triangle class below!
+class Triangle(Shape):
+  def __init__(self, side1, side2, side3):
+    self.side1 = side1
+    self.side2 = side2
+    self.side3 = side3
