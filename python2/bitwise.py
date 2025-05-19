7040b56772e37637e8e01dbece05034df47f37eb
Lalo <114935993+lalolel@users.noreply.github.com>
2025-05-19 15:40:57 +0300

    Update bitwise.py

--- python2/bitwise.py
+++ python2/bitwise.py
@@ -4,3 +4,59 @@ print 8 & 5   # Bitwise AND
 print 9 | 4   # Bitwise OR
 print 12 ^ 42 # Bitwise XOR
 print ~88     # Bitwise NOT
+
+print 0b1,    #1
+print 0b10,   #2
+print 0b11,   #3
+print 0b100,  #4
+print 0b101,  #5
+print 0b110,  #6
+print 0b111   #7
+print "******"
+print 0b1 + 0b11
+print 0b11 * 0b11
+
+8's bit  4's bit  2's bit  1's bit
+    1       0       1      0 
+    8   +   0    +  2   +  0  = 10 
+
+2 ** 0 = 1
+2 ** 1 = 2
+2 ** 2 = 4
+2 ** 3 = 8
+2 ** 4 = 16
+2 ** 5 = 32
+2 ** 6 = 64
+2 ** 7 = 128
+2 ** 8 = 256
+2 ** 9 = 512
+2 ** 10 = 1024
+
+one = 0b1
+two = 0b10
+three = 0b11
+four = 0b100
+five = 0b101
+six  = 0b110
+seven = 0b111
+eight = 0b1000
+nine  = 0b1001
+ten = 0b1010
+eleven = 0b1011
+twelve = 0b1100
+
+print bin(1)
+print bin(2)
+print bin(3)
+print bin(4)
+print bin(5)
+
+''' In the console are several different ways that you can use the int function’s second parameter.On line 7, use int to print the base 10 equivalent of the binary number 11001001. '''
+
+print int("1",2)
+print int("10",2)
+print int("111",2)
+print int("0b100",2)
+print int(bin(5),2)
+# Print out the decimal equivalent of the binary 11001001.
+print int("11001001", 2)
