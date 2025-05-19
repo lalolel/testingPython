fb907f7d54e57a75be4791eca9f7f192c2ff73f1
Lalo <114935993+lalolel@users.noreply.github.com>
2025-05-19 15:12:44 +0300

    Update dictionaries.py

--- python2/dictionaries.py
+++ python2/dictionaries.py
@@ -148,3 +148,25 @@ str[start:end:stride]
 garbled = "!XeXgXaXsXsXeXmX XtXeXrXcXeXsX XeXhXtX XmXaX XI"
 
 message = garbled[::-2]
+
+
+''' Last but not least, let’s look over some 
+Preview: Docs Loading link description
+lambdas.
+
+my_list = range(16)
+filter(lambda x: x % 3 == 0, my_list) '''
+
+''' Create a new variable called message.
+
+Set it to the result of calling filter() with the appropriate lambda that will filter out the "X"s. The second argument will be garbled.
+
+Finally, print your message to the console. 
+
+We’ve given you another (slightly different) garbled. Sort it out with a filter() and a lambda.  '''
+
+
+garbled = "IXXX aXXmX aXXXnXoXXXXXtXhXeXXXXrX sXXXXeXcXXXrXeXt mXXeXsXXXsXaXXXXXXgXeX!XX"
+
+message = filter(lambda x: x != "X", garbled)
+print message
