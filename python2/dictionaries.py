45412d85b09251f3563fd254948d420a64d8c93b
Lalo <114935993+lalolel@users.noreply.github.com>
2025-05-19 14:42:34 +0300

    Update dictionaries.py

--- python2/dictionaries.py
+++ python2/dictionaries.py
@@ -113,3 +113,11 @@ languages = ["HTML", "JavaScript", "Python", "Ruby"]
 
 # Add arguments to the filter()
 print filter(lambda x: x == "Python", languages)
+
+''' Create a list, squares, that consists of the squares of the numbers 1 to 10. A list comprehension could be useful here!
+
+Use filter() and a lambda expression to print out only the squares that are between 30 and 70 (inclusive). '''
+
+squares = [x ** 2 for x in range(1, 11)]
+
+print filter(lambda x: x >= 30 and x <= 70, squares)
