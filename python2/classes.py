94ab3e597bf6395a4d90e4000215398a2d8a2200
Lalo <114935993+lalolel@users.noreply.github.com>
2025-05-19 17:24:17 +0300

    Update classes.py

--- python2/classes.py
+++ python2/classes.py
@@ -19,3 +19,12 @@ lemon = Fruit("lemon", "yellow", "sour", False)
 
 lemon.description()
 lemon.is_edible()
+
+''' Create a class called Animal in the editor. For now, in the body of your class, use the pass keyword. (pass doesn’t do anything, but it’s useful as a placeholder in areas of your code where Python expects an expression.) '''
+'''     started our class definition off with an odd-looking function: __init__(). 
+This function is required for classes, and it’s used to initialize the objects it creates. __init__() always takes at least one argument, self, that refers to the object being created. 
+You can think of __init__() as the function that “boots up” each object the class creates. '''
+
+class Animal(object):
+  def __init__(self):
+    pass
