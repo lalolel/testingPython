55d698c3e16c204ec94e7843b86cd86eb68d5676
Lalo <114935993+lalolel@users.noreply.github.com>
2025-05-22 19:40:17 +0300

    Update magic8ball.py

--- magic8Ball/magic8ball.py
+++ magic8Ball/magic8ball.py
@@ -1,12 +1,20 @@
+# Import the random module to generate random numbers
 import random
 
+# Set up the person asking the question and their question
 name = "Joe"
 question = "Will I win the lottery?"
+
+# Initialize an empty string to store the Magic 8 Ball's answer
 answer = ""
 
+# Generate a random integer between 1 and 9 (inclusive)
 random_number = random.randint(1, 9)
+
+# Uncomment the line below if you want to see what random number was generated
 #print(random_number)
 
+# Use conditional statements to assign an answer based on the random number
 if random_number == 1:
   answer = "Yes - definitely"
 elif random_number == 2:
@@ -26,7 +34,11 @@ elif random_number == 8:
 elif random_number == 9:
   answer = "Very doubtful"
 else:
+  # This else clause handles any unexpected values (though shouldn't occur with randint(1,9))
   answer = "Error"
-  
+
+# Display the question that was asked
 print(name + " asks: " + question)
+
+# Display the Magic 8 Ball's randomly selected answer
 print("Magic 8 Ball's answer: " + answer)
