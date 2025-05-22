cd38ed6071167b6cd9017da752033077bdf0fbe1
Lalo <114935993+lalolel@users.noreply.github.com>
2025-05-22 21:22:38 +0300

    Update listComp.py

--- RCS/listComp.py
+++ RCS/listComp.py
@@ -27,6 +27,15 @@ print(numbers)  # Output: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 squares = [x**2 for x in range(5)]
 print(squares)  # Output: [0, 1, 4, 9, 16]
 
+# Original list of numbers
+numbers = [1, 2, 3, 4, 5]
+
+# Using list comprehension to add 10 to each element
+new_numbers = [num + 10 for num in numbers]
+
+# Printing the new list
+print(new_numbers)
+
 
 #3. Filtering elements
 #Select only even numbers from a list:
