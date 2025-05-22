8dd89f2a80d5dc4788ba9d3519da02a6ef6be061
Lalo <114935993+lalolel@users.noreply.github.com>
2025-05-22 20:47:33 +0300

    Create 5.py

---
+++ RCS/5.py
@@ -0,0 +1,17 @@
+# f strings
+# v1
+name = "Lalo"  # Replace with any name
+age = 25  # Replace with any age
+
+print(f"Hi, my name is {name} and I'm {age} years old.")
+
+age += 1  # Increment age by 1
+print(f"Next year, I'll be {age} years old.")
+
+
+# v2
+name = "Lalo"
+age = 25
+
+print(f"Hi, my name is {name} and I'm {age} years old.")
+print(f"Next year, I'll be {age + 1} years old.")
