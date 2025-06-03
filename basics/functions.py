ff926f27863e1135e2045b1325c0a3333063d231
Lalo <114935993+lalolel@users.noreply.github.com>
2025-06-03 23:32:04 +0300

    Update functions.py

--- basics/functions.py
+++ basics/functions.py
@@ -22,3 +22,24 @@ monday, tuesday, wednesday = threeday_weather_report(weather_data)
 print(monday)
 print(tuesday)
 print(wednesday)
+
+
+def trip_planner_welcome(name): 
+  print("Welcome to tripplanner v1.0 " + name)
+
+trip_planner_welcome(" <YOUR NAME HERE> ")
+
+def estimated_time_rounded(estimated_time):
+  rounded_time = round(estimated_time)
+  return rounded_time
+
+estimate = estimated_time_rounded(2.43)
+
+def destination_setup(origin, destination, estimated_time, mode_of_transport="Car"):
+  print("Your trip starts off in " + origin)
+  print("And you are traveling to " + destination)
+  print("You will be traveling by " + mode_of_transport)
+  print("It will take approximately " + str(estimated_time) + " hours")
+
+
+destination_setup(" <PICK AN ORIGIN> ", "<PICK A DESTINATION > ", estimate, "Car")
