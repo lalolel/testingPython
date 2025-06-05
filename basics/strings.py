5dac34b5f21738394c3c47eee383de5b5b2266fd
Lalo <114935993+lalolel@users.noreply.github.com>
2025-06-06 00:31:27 +0300

    Update strings.py

--- basics/strings.py
+++ basics/strings.py
@@ -29,3 +29,23 @@ def common_letters(string_one, string_two):
     if (letter in string_two) and not (letter in common):
       common.append(letter)
   return common
+
+
+
+def username_generator(first_name, last_name):
+    if len(first_name) < 3:
+        user_name = first_name
+    else:
+        user_name = first_name[0:3]
+    if len(last_name) < 4:
+        user_name += last_name
+    else:
+        user_name += last_name[0:4]
+    return user_name
+  
+    
+def password_generator(user_name):
+    password = ""
+    for i in range(0, len(user_name)):
+        password += user_name[i-1]
+    return password
