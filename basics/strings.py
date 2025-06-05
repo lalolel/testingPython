83ad6740931dbe6c17b41511cd0dc08dd1ac80e7
Lalo <114935993+lalolel@users.noreply.github.com>
2025-06-06 00:07:21 +0300

    Create strings.py

---
+++ basics/strings.py
@@ -0,0 +1,31 @@
+first_name = "Julie"
+last_name = "Blevins"
+
+def account_generator(first_name, last_name):
+  account_name = first_name[:3] + last_name[:3]
+  return account_name
+
+new_account = account_generator(first_name, last_name)
+
+print(new_account)
+
+first_name = "Reiko"
+last_name = "Matsuki"
+
+def password_generator(first_name, last_name):
+  temp_password = first_name[len(first_name)-3:] + last_name[len(last_name)-3:]
+  return temp_password
+
+temp_password = password_generator(first_name, last_name)
+
+password = "theycallme\"crazy\"91"
+
+def contains(big_string, little_string):
+  return little_string in big_string
+
+def common_letters(string_one, string_two):
+  common = []
+  for letter in string_one:
+    if (letter in string_two) and not (letter in common):
+      common.append(letter)
+  return common
