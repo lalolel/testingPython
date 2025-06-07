7d2c25fd6198580e723228d7327f94715d1e631e
Lalo <114935993+lalolel@users.noreply.github.com>
2025-06-07 14:45:08 +0300

    Update usingDicts.py

--- basics/usingDicts.py
+++ basics/usingDicts.py
@@ -45,3 +45,9 @@ total_exercises = 0
 for exercises in num_exercises.values():
   total_exercises += exercises
 print(total_exercises)
+
+
+pct_women_in_occupation = {"CEO": 28, "Engineering Manager": 9, "Pharmacist": 58, "Physician": 40, "Lawyer": 37, "Aerospace Engineer": 9}
+
+for occupation, percentage in pct_women_in_occupation.items():
+  print("Women make up " + str(percentage) + " percent of " + occupation + "s.") 
