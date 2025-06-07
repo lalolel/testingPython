1de6de6ec5731a6263b2a20f30fc47ecab4084e7
Lalo <114935993+lalolel@users.noreply.github.com>
2025-06-07 14:51:21 +0300

    Update usingDicts.py

--- basics/usingDicts.py
+++ basics/usingDicts.py
@@ -1,3 +1,6 @@
+
+
+# 1. Dictionary basics
 zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "Leo", "Sagittarius"], "earth": ["Taurus", "Virgo", "Capricorn"], "air":["Gemini", "Libra", "Aquarius"]}
 
 print(zodiac_elements['earth'])
@@ -10,7 +13,7 @@ zodiac_elements["energy"] = "Not a Zodiac element"
 if "energy" in zodiac_elements:
   print(zodiac_elements["energy"])
 
-
+# 2. 
 user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
 
 tc_id = user_ids.get("teraCoder", 100000)
@@ -18,7 +21,7 @@ stack_id = user_ids.get("superStackSmash", 100000)
 
 print(tc_id)
 print(stack_id)
-
+# 3.  
 available_items = {"health potion": 10, "cake of the cure": 5, "green elixir": 20, "strength sandwich": 25, "stamina grains": 15, "power stew": 30}
 health_points = 20
 
@@ -28,7 +31,7 @@ health_points += available_items.pop("mystic bread", 0)
 
 print(available_items)
 print(health_points)
-
+# 4. 
 user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
 num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
 
@@ -37,7 +40,7 @@ lessons = num_exercises.keys()
 
 print(users)
 print(lessons)
-
+# 5. 
 num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
 
 total_exercises = 0
@@ -46,8 +49,21 @@ for exercises in num_exercises.values():
   total_exercises += exercises
 print(total_exercises)
 
-
+# 6. 
 pct_women_in_occupation = {"CEO": 28, "Engineering Manager": 9, "Pharmacist": 58, "Physician": 40, "Lawyer": 37, "Aerospace Engineer": 9}
 
 for occupation, percentage in pct_women_in_occupation.items():
   print("Women make up " + str(percentage) + " percent of " + occupation + "s.") 
+
+# 7.
+
+tarot = { 1:	"The Magician", 2:	"The High Priestess", 3:	"The Empress", 4:	"The Emperor", 5:	"The Hierophant", 6:	"The Lovers", 7:	"The Chariot", 8:	"Strength", 9:	"The Hermit", 10:	"Wheel of Fortune", 11:	"Justice", 12:	"The Hanged Man", 13:	"Death", 14:	"Temperance", 15:	"The Devil", 16:	"The Tower", 17:	"The Star", 18:	"The Moon", 19:	"The Sun", 20:	"Judgement", 21:	"The World", 22: "The Fool"}
+
+spread = {}
+
+spread["past"] = tarot.pop(13)
+spread["present"] = tarot.pop(22)
+spread["future"] = tarot.pop(10)
+
+for key, value in spread.items():
+  print("Your "+key+" is the "+value+" card. ")
